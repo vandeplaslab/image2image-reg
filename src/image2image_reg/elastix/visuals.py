f1f26376f5831249b8da1f0de931bbb185fb8bee
Lukasz Migas <lukas.migas@yahoo.com>
2024-12-17 11:57:57 +0000

    Update visuals.py

--- src/image2image_reg/elastix/visuals.py
+++ src/image2image_reg/elastix/visuals.py
@@ -6,27 +6,13 @@ import typing as ty
 
 import matplotlib.pyplot as plt
 import networkx as nx
+from koyo.color import get_next_color
 from matplotlib.patches import ArrowStyle
 
 from image2image_reg.enums import NetworkTypes
 
 if ty.TYPE_CHECKING:
     from image2image_reg.workflows import ElastixReg
-
-COLOR_MAP = [
-    "#a6cee3",
-    "#1f78b4",
-    "#b2df8a",
-    "#33a02c",
-    "#fb9a99",
-    "#e31a1c",
-    "#fdbf6f",
-    "#ff7f00",
-    "#cab2d6",
-    "#6a3d9a",
-    "#ffff99",
-    "#b15928",
-]
 
 
 def draw_registration_nodes(
@@ -53,10 +39,7 @@ def draw_registration_nodes(
     # Add nodes
     g = nx.DiGraph()
     for idx, modality in enumerate(workflow.modalities):
-        if idx > 11:
-            color = COLOR_MAP[idx - 11]
-        else:
-            color = COLOR_MAP[idx]
+        color = get_next_color(idx)
         g.add_node(modality, color=color, cstyle=f"arc3,rad=-{(idx+1)*0.08+0.05}")
 
     for src, tgts in workflow.registration_paths.items():
@@ -65,9 +48,15 @@ def draw_registration_nodes(
     pos = nx.kamada_kawai_layout(g)
     pos = {p: v * 20 for p, v in pos.items()}
     nx.draw_networkx_nodes(
-        g, pos, label=g.nodes, node_color=[g.nodes[n]["color"] for n in g.nodes], node_size=node_size, alpha=node_alpha
+        g,
+        pos,
+        label=g.nodes,
+        node_color=[g.nodes[n]["color"] for n in g.nodes],
+        node_size=node_size,
+        alpha=node_alpha,
     )
 
+    # add text to the nodes
     for src, tgts in workflow.registration_paths.items():
         src_color = g.nodes[src]["color"]
         cstyle = g.nodes[src]["cstyle"]
@@ -131,8 +120,6 @@ def draw_workflow(
 
     if ax is None:
         fig, ax = plt.subplots()
-    else:
-        pass
 
     ax.clear()
     ax.axis("off")
@@ -143,25 +130,26 @@ def draw_workflow(
 
     for idx, modality in enumerate(workflow.modalities):
         rad = (idx + 1) * 0.08 + 0.05
-        if idx > 11:
-            color = COLOR_MAP[idx - 11]
-        else:
-            color = COLOR_MAP[idx]
+        color = get_next_color(idx)
 
         g.add_node(modality, color=color, cstyle=f"arc3,rad=-{rad}")
         g_layout.add_node(modality, color=color, cstyle=f"arc3,rad=-{rad}")
 
-    for attachment_name, attachment_mod in workflow.attachment_images.items():
-        g.add_node(attachment_name, color="green")
-        g_layout.add_edge(attachment_name, attachment_mod)
+    legend = {}
+    for attach_to, attached_name in workflow.attachment_images.items():
+        g.add_node(attach_to, color="green")
+        g_layout.add_edge(attach_to, attached_name)
+        legend["attached image"] = {"markerfacecolor": "green", "marker": "o"}
 
     for shape_set_name, shape_info in workflow.attachment_shapes.items():
         g.add_node(shape_set_name, color="red")
         g_layout.add_edge(shape_set_name, shape_info["attach_to"])
+        legend["attached shape"] = {"markerfacecolor": "red", "marker": "o"}
 
-    for shape_set_name, shape_info in workflow.attachment_points.items():
-        g.add_node(shape_set_name, color="yellow")
-        g_layout.add_edge(shape_set_name, shape_info["attach_to"])
+    for point_set_name, point_info in workflow.attachment_points.items():
+        g.add_node(point_set_name, color="yellow")
+        g_layout.add_edge(point_set_name, point_info["attach_to"])
+        legend["attached points"] = {"markerfacecolor": "yellow", "marker": "o"}
 
     for source, targets in workflow.registration_paths.items():
         g.add_edge(source, targets[0])
@@ -191,7 +179,6 @@ def draw_workflow(
     else:
         raise ValueError(f"Invalid layout: {layout}")
 
-    # pos = {p: v * 20 for p, v in pos.items()}
     nx.draw_networkx_nodes(
         g,
         pos,
@@ -202,12 +189,12 @@ def draw_workflow(
         ax=ax,
     )
 
-    for attachment_name, attachment_mod in workflow.attachment_images.items():
+    for attach_to, attached_name in workflow.attachment_images.items():
         ax.annotate(
             "",
-            xy=pos[attachment_name],
+            xy=pos[attach_to],
             xycoords="data",
-            xytext=pos[attachment_mod],
+            xytext=pos[attached_name],
             textcoords="data",
             arrowprops={
                 "arrowstyle": ArrowStyle.BracketA(widthA=0.5),
@@ -284,6 +271,7 @@ def draw_workflow(
         path_targets = nx.algorithms.shortest_path(g, source, targets[-1])[1:]
 
         if len(path_targets) > 1:
+            legend["through modality"] = {"color": "white", "ls": "dotted"}
             for idx, cont_src in enumerate(path_targets[:-1]):
                 current, target = cont_src, path_targets[idx + 1]
                 ax.annotate(
@@ -318,3 +306,20 @@ def draw_workflow(
         font_color="white",
         font_size=9.5,
     )
+
+    # add legend for node colors
+    if legend:
+        names, handles = [], []
+        for name, kws in legend.items():
+            kws.setdefault("color", "white")
+            handles.append(plt.Line2D([0], [0], markersize=10, **kws))
+            names.append(name)
+
+        ax.legend(
+            handles=handles,
+            labels=names,
+            loc="upper left",
+            fontsize="small",
+            labelcolor="white",
+            frameon=False,
+        )
