afe96a08d38ace8e3888cd1bfcfe9059e5f664ce
Lukasz Migas <lukas.migas@yahoo.com>
2025-11-19 17:32:17 +0000

    Update __init__.py

--- src/image2image_reg/__init__.py
+++ src/image2image_reg/__init__.py
@@ -1,10 +1,8 @@
 """Whole slide image registration using elastix."""
 
-from importlib.metadata import PackageNotFoundError, version
-
 from loguru import logger
 
-__version__ = "0.1.12"
+__version__ = "0.2.1"
 __author__ = "Lukasz G. Migas"
 __email__ = "lukas.migas@yahoo.com"
 
